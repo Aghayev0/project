e08a8a596fe250a8af5e69d184b2bc6dfad6f956
Aghayev0 <tu6k6wzh2@code.edu.az>
2023-02-16 21:55:05 +0400

    feat:added backend and connected to database

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+require("dotenv").config();
+app.use(express.json());
+const dbConfig = require('./config/dbConfig')
+
+const usersRoute = require('./routes/usersRoute');
+
+app.use('/api/users', usersRoute);
+const PORT = process.env.PORT || 8080
+app.listen(PORT, ()=>{
+    console.log(`Server listening on ${PORT}`);
+})
